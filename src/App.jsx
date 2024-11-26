a7cf4ba780a828fe98db5df37ef5452983b88107
huyhtf88188 <huyhtf8188@fullstack.edu.vn>
2024-11-26 19:37:14 +0700

    nop bai

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-// import { useEffect, useState } from "react";
-import "./App.scss";
-
 import Footer from "./components/footer/Footer";
-import Header from "./components/Header";
-import Shop from "./pages/Shop";
+import Header from "./components/header/Header";
+import Shop from "./components/pages/Shop";
 
 function App() {
   return (
