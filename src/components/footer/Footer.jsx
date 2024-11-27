529cbf1c597af653ef4680f1a10442f7e48b0314
huyhtf88188 <huyhtf8188@fullstack.edu.vn>
2024-11-27 13:54:49 +0700

    u

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+import style from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <p>Copyright by HH</p>
+      <button id={style.abc}>Folow Me</button>
+    </footer>
+  );
+};
+export default Footer;
