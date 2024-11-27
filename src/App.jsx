e6951c3ceb2876b0096c26d69eacb755db8d4dff
huyhtf88188 <huyhtf8188@fullstack.edu.vn>
2024-11-27 13:57:04 +0700

    u

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
-import Shop from "./components/pages/Shop";
+import Shop from "./pages/Shop";
 
 function App() {
   return (
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,8 @@
-// import React from "react";
-import style from "./Footer.module.scss";
-
 const Footer = () => {
   return (
     <footer id="footer">
       <p>Copyright by HH</p>
-      <button id={style.abc}>Folow Me</button>
+      <button>Folow Me</button>
     </footer>
   );
 };
