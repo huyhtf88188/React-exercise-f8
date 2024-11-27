cd8e530e5919b96a6cdc1642b5e43261a790674f
huyhtf88188 <huyhtf8188@fullstack.edu.vn>
2024-11-27 13:30:28 +0700

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import Footer from "./components/footer/Footer";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Shop from "./components/pages/Shop";
 
@@ -8,7 +8,7 @@ function App() {
       <Header />
       <Shop />
 
-      {/* <Footer2 /> */}
+      <Footer />
     </>
   );
 }
